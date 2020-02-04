dac1d2e5705f444855d908ea156c2cfd7f25de55
Talha Ekhlas <ekhlas.cse08@gmail.com>
2020-02-04 15:44:32 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'antd';
+import './App.css';
+import './test.scss';
+
+const App = () => (
+  <div className="App">
+    <Button type="primary test">Button</Button>
+  </div>
+);
+
+export default App;
